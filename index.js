f896f6f4d81564d9e66031e9246ac9a26dedb946
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-03-06 18:28:16 +0900

    Add scripts

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Loader
+
+import core from "@actions/core"
+import { getOctokit, Context } from "@actions/github"
+import { run } from "./src/main"
+
+const token = core.getInput("token", {required:true})
+const octokit = getOctokit(token)
+const message = core.getInput("message")
+
+const context = Context
+
+try {
+  run(core, octokit, context, message)
+} catch (e) {
+  core.setFailed(`==> ${e.message}`)
+}
---
+++ src/main.js
@@ -0,0 +1,24 @@
+
+/**
+ * 
+ * @param {import("@actions/core")} core 
+ * @param {import("@actions/github").context} context
+ * @param {ReturnType<typeof import("@actions/github").getOctokit>} octokit 
+ * @param {String} message 
+ */
+
+export async function run(core, octokit, context, message) {
+  core.info(`==> Auto Approving #${context.payload.pull_request.number}`)
+  try {
+    await octokit.rest.pulls.createReview({
+      ...context.repo,
+      pull_number: context.payload.pull_request.number,
+      event: "APPROVE",
+      comments: {
+        body: message
+      }
+    })
+  } catch (e) {
+    core.setFailed(`==> ${e.message}`)
+  }
+}
